3c4051dcd3d52d5ec01361e8ec32cd5a5580a613
Maz <mazoolander@me.com>
2024-11-22 11:02:18 +0100

    Scroll To Top Button

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,29 @@
+// Navbar Scroll Effect
+
 function userScroll() {
   const navbar = document.querySelector(".navbar");
+  const toTopBtn = document.querySelector("#to-top");
 
   window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
       navbar.classList.add("bg-dark");
       navbar.classList.add("navbar-sticky");
+      toTopBtn.classList.add("show");
     } else {
       navbar.classList.remove("bg-dark");
       navbar.classList.remove("navbar-sticky");
+      toTopBtn.classList.remove("show");
     }
   });
 }
 
-// run when DOM is loaded
+// Go to top
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// run when DOM is loaded / Event Listeners
 document.addEventListener("DOMContentLoaded", userScroll);
+document.querySelector("#to-top").addEventListener("click", scrollToTop);
